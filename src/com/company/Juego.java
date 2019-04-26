651b92b44faa2cf2371e94d6ffaecf48356ef95e
Yago Morales <yagomoralesares@gmail.com>
2019-04-26 19:21:56 +0200

    Jugadas comer y bien

--- src/com/company/Juego.java
+++ src/com/company/Juego.java
@@ -7,53 +7,23 @@ public class Juego {
     int id;
     boolean finished, correctplay;
     Scanner scanner = new Scanner(System.in);
+    int comprobacion;
 
-    public Juego(){
+    public Juego() {
         tablero = new Tablero();
-        id = 2;
+        id = 3;
     }
 
-    public void jugar(){
+    public void movimiento() {
         do {
             do {
                 tablero.dibujarTablero();
-                System.out.println("\u001B[0m"+"\nQue ficha quieres mover?");
+                System.out.println("\u001B[0m" + "\nQue ficha quieres mover?");
                 System.out.println("Introduce la columna");
                 char col = scanner.next().toLowerCase().charAt(0);
                 int numbercol = 0;
 
-                switch (col){
-                    case 'a':
-                        numbercol = 0;
-                        break;
-                    case 'b':
-                        numbercol = 1;
-                        break;
-                    case 'c':
-                        numbercol = 2;
-                        break;
-                    case 'd':
-                        numbercol = 3;
-                        break;
-                    case 'e':
-                        numbercol = 4;
-                        break;
-                    case 'f':
-                        numbercol = 5;
-                        break;
-                    case 'g':
-                        numbercol = 6;
-                        break;
-                    case 'h':
-                        numbercol = 7;
-                        break;
-                    case 'i':
-                        numbercol = 8;
-                        break;
-                    case 'j':
-                        numbercol = 9;
-                        break;
-                }
+                numbercol = getNumbercol(col, numbercol);
                 System.out.println("Introduce la fila");
                 int fila = scanner.nextInt() - 1;
                 scanner.nextLine();
@@ -62,64 +32,121 @@ public class Juego {
                 char newcol = scanner.next().toLowerCase().charAt(0);
                 int newnumbercol = 0;
 
-                switch (newcol){
-                    case 'a':
-                        newnumbercol = 0;
-                        break;
-                    case 'b':
-                        newnumbercol = 1;
-                        break;
-                    case 'c':
-                        newnumbercol = 2;
-                        break;
-                    case 'd':
-                        newnumbercol = 3;
-                        break;
-                    case 'e':
-                        newnumbercol = 4;
-                        break;
-                    case 'f':
-                        newnumbercol = 5;
-                        break;
-                    case 'g':
-                        newnumbercol = 6;
-                        break;
-                    case 'h':
-                        newnumbercol = 7;
-                        break;
-                    case 'i':
-                        newnumbercol = 8;
-                        break;
-                    case 'j':
-                        newnumbercol = 9;
-                        break;
-                }
+                newnumbercol = getNumbercol(newcol, newnumbercol);
                 System.out.println("Introduce la fila");
-                int newfila = scanner.nextInt()-1;
+                int newfila = scanner.nextInt() - 1;
                 scanner.nextLine();
+                if (checkPropietario(numbercol, fila, newnumbercol, newfila) &&checkMovimiento(numbercol, fila, newnumbercol, newfila)) {
 
+                        tablero.getMesa()[fila][numbercol] = tablero.CASROJA;
+                        tablero.getMesa()[newfila][newnumbercol] = id;
+                        correctplay = true;
+                        if (id == tablero.FICAZUL)id = tablero.FICAMARILLO;
+                        else id=tablero.FICAZUL;
 
-                    if (tablero.getMesa()[fila][numbercol] == tablero.CASGRIS || tablero.getMesa()[fila][numbercol] == tablero.CASROJA) {
-                        System.out.println("No hay ninguna ficha en esa posición");
-                    }
-                    else if (tablero.getMesa()[fila][numbercol] != id){
-                        System.out.println("Esa ficha no es tuya");
-                    }
-                    else if (tablero.getMesa()[fila][numbercol] == id){
-                        if (tablero.getMesa()[newfila][newnumbercol] != tablero.CASROJA) {
-                            System.out.println("No puedes mover ahi");
-                        }
-                        else if (tablero.getMesa()[newfila][newnumbercol] == tablero.CASROJA) {
-                            tablero.getMesa()[fila][numbercol] = tablero.CASROJA;
-                            tablero.getMesa()[newfila][newnumbercol] = id;
-                            correctplay = true;
-                        }
-                    }
                 }
-                while(!correctplay);
+            }
+
+            while (!correctplay);
 
         }
-        while (!finished);
+        while (!tablero.checkWinner());
+    }
+
+    private int getNumbercol(char col, int numbercol) {
+        switch (col) {
+            case 'a':
+                numbercol = 0;
+                break;
+            case 'b':
+                numbercol = 1;
+                break;
+            case 'c':
+                numbercol = 2;
+                break;
+            case 'd':
+                numbercol = 3;
+                break;
+            case 'e':
+                numbercol = 4;
+                break;
+            case 'f':
+                numbercol = 5;
+                break;
+            case 'g':
+                numbercol = 6;
+                break;
+            case 'h':
+                numbercol = 7;
+                break;
+            case 'i':
+                numbercol = 8;
+                break;
+            case 'j':
+                numbercol = 9;
+                break;
+        }
+        return numbercol;
+    }
+
+    private boolean checkMovimiento(int numbercol, int fila, int newnumbercol, int newfila) {
+        comprobacion = fila - newfila;
+//        if (id == tablero.FICAMARILLO){
+//            comprobacion = -comprobacion;
+//        }
+        if ((id==tablero.FICAMARILLO && comprobacion<0)|| (id==tablero.FICAZUL && comprobacion>0) ) {
+            if (numbercol - newnumbercol == comprobacion || numbercol - newnumbercol == -comprobacion) {
+                if (comprobacion == 2 || comprobacion == -2) {
+                    if (comprobacion < 0) {
+                        if ((tablero.getMesa()[fila - comprobacion/2][numbercol - (newnumbercol- numbercol)/2] == tablero.FICAZUL
+                                || tablero.getMesa()[fila - comprobacion/2][numbercol + (newnumbercol- numbercol)/2] == tablero.FICAZUL)
+                                && tablero.getMesa()[newfila][newnumbercol] == tablero.CASROJA) {
+                            int borrar = (newnumbercol - numbercol) /2;
+                            tablero.getMesa()[fila-comprobacion/2][numbercol+borrar]=tablero.CASROJA;
+                            System.out.println("correcto");
+                            return true;
+                        }
+                    } else if ((tablero.getMesa()[fila - comprobacion/2][numbercol - (newnumbercol- numbercol)/2] == tablero.FICAMARILLO
+                            || tablero.getMesa()[fila - comprobacion/2][numbercol + (newnumbercol- numbercol)/2] == tablero.FICAMARILLO)
+                            && tablero.getMesa()[newfila][newnumbercol] == tablero.CASROJA) {
+                        int borrar = (newnumbercol - numbercol) /2;
+                        tablero.getMesa()[fila-comprobacion/2][numbercol+borrar]=tablero.CASROJA;
+                        System.out.println("correcto");
+                        return true;
+                    } else {
+                        System.out.println("No puedes moverte dos no hay nada que comer");
+                        return false;
+                    }
+                } else if (comprobacion == 1 || comprobacion == -1) {
+                    if (tablero.getMesa()[newfila][newnumbercol] == tablero.CASROJA) System.out.println("correcto");
+                    return true;
+                } else {
+                    System.out.println("La distancia no es correcta");
+                    return false;
+                }
+            }
+        }else {
+            System.out.println("No te puedes mover hacia atras");
+        }
+
+        System.out.println("mal");
+        return false;
+    }
+
+    private boolean checkPropietario(int numbercol, int fila, int newnumbercol, int newfila) {
+        if (tablero.getMesa()[fila][numbercol] == tablero.CASGRIS || tablero.getMesa()[fila][numbercol] == tablero.CASROJA) {
+            System.out.println("No hay ninguna ficha en esa posición");
+            return false;
+        } else if (tablero.getMesa()[fila][numbercol] != id) {
+            System.out.println("Esa ficha no es tuya");
+            return false;
+        } else if (tablero.getMesa()[fila][numbercol] == id) {
+            if (tablero.getMesa()[newfila][newnumbercol] != tablero.CASROJA) {
+                System.out.println("No puedes mover ahi");
+                return false;
+            }
+        }
+        return true;
     }
 
 }
